d2c928e8ed0c8350445e68fba87a76341c4a7303
nazmul162001 <nazmulprime162001@gmail.com>
2022-02-22 20:06:50 +0600

    add post & get post from server with custom style

--- js/api.js
+++ js/api.js
@@ -20,9 +20,25 @@ function loadPost(){
 function displayUsers(data){
   const ul = document.getElementById('users');
   for (const user of data) {
-    console.log(user.name);
     const li = document.createElement('li');
     li.innerText = ` name: ${user.name} email: ${user.email}`;
     ul.appendChild(li);
   }
+}
+
+// creat a post 
+function getAPost(){
+  fetch('https://jsonplaceholder.typicode.com/posts',{
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'My new post',
+      body: 'This is my Posts',
+      userId: 1
+    }),
+    headers:{
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
+  .then(res => res.json())
+  .then(data => console.log(data));
 }
